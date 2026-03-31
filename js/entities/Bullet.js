47b18bf8bb9b8895845f84b72840d52985feb518
Glackso <jabrokecool@gmail.com>
2026-03-31 15:26:33 -0500

    Update Bullet.js

--- js/entities/Bullet.js
+++ js/entities/Bullet.js
@@ -1,27 +1,34 @@
 import { Entity } from './Entity.js';
 
 export class Bullet extends Entity {
-    constructor(x, y, angle) {
-        super(x, y); // Calls the Entity constructor
-        this.speed = 8;
-        this.vx = Math.cos(angle) * this.speed;
-        this.vy = Math.sin(angle) * this.speed;
-        this.life = 100; // Frames before it disappears
+    constructor(x, y, angle, speed, damage, color) {
+        super(x, y, 10, color); 
+        this.vx = Math.cos(angle) * speed;
+        this.vy = Math.sin(angle) * speed;
+        
+        this.damage = damage;
+        this.maxLife = 100;
+        this.life = this.maxLife;
     }
-    
+
     update() {
-        this.x += this.vx;
-        this.y += this.vy;
+        super.update(); // Applies vx/vy to x/y
         this.life--;
     }
-    
+
     draw(ctx) {
-        ctx.fillStyle = '#f14e54';
-        ctx.strokeStyle = '#555';
-        ctx.lineWidth = 2;
+        // Fade out smoothly based on remaining life
+        ctx.globalAlpha = Math.max(0, this.life / this.maxLife); 
+        
+        ctx.fillStyle = this.color;
+        ctx.strokeStyle = this.outlineColor;
+        ctx.lineWidth = this.lineWidth;
+        
         ctx.beginPath();
-        ctx.arc(this.x, this.y, 8, 0, Math.PI * 2);
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
         ctx.fill();
         ctx.stroke();
+        
+        ctx.globalAlpha = 1.0; // Always reset transparency!
     }
 }
