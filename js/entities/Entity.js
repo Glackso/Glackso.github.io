3c0600e2b05ba8b93cc97576d7cf5e1c9b248b09
Glackso <jabrokecool@gmail.com>
2026-03-31 15:26:04 -0500

    Update Entity.js

--- js/entities/Entity.js
+++ js/entities/Entity.js
@@ -1,8 +1,33 @@
 export class Entity {
-    constructor(x, y) {
+    constructor(x, y, radius, color) {
         this.x = x;
         this.y = y;
+        this.vx = 0;
+        this.vy = 0;
+        this.radius = radius;
+        this.color = color;
+        
+        this.health = 100;
+        this.maxHealth = 100;
+        
+        // Universal style properties for this game
+        this.outlineColor = '#555555';
+        this.lineWidth = 3.5;
     }
-    update() {}
-    draw(ctx) {}
+
+    update() {
+        this.x += this.vx;
+        this.y += this.vy;
+    }
+
+    draw(ctx) {
+        // Base draw just draws a colored circle (overridden by specific classes)
+        ctx.fillStyle = this.color;
+        ctx.strokeStyle = this.outlineColor;
+        ctx.lineWidth = this.lineWidth;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        ctx.fill();
+        ctx.stroke();
+    }
 }
