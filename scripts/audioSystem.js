5860040669c197e5d35555490e52b3c4905ae3d4
Glackso <jabrokecool@gmail.com>
2026-04-19 21:59:38 -0500

    Update audioSystem.js

--- scripts/audioSystem.js
+++ scripts/audioSystem.js
@@ -1,1 +1,12 @@
-
+const AudioEngine = {
+    play: function(soundName) {
+        let file = '';
+        if (soundName === 'startup') file = 'assets/sounds/startup.wav';
+        if (soundName === 'click') file = 'assets/sounds/click.wav'; // For future use
+        
+        if (file) {
+            const audio = new Audio(file);
+            audio.play().catch(e => console.log("Browser blocked auto-play until user clicks."));
+        }
+    }
+};
