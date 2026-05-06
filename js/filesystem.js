63b7ac8490f84e6273acb0a88b0f4b2f0b885b8a
Glackso <jabrokecool@gmail.com>
2026-05-06 08:35:53 -0500

    Update filesystem.js

--- js/filesystem.js
+++ js/filesystem.js
@@ -1,1 +1,7 @@
+const driveC = {
+    "C:\\": ["Windows", "Documents", "Program Files"],
+    "C:\\Windows": ["System32", "Web", "Media"],
+    "C:\\Documents": ["My Pictures", "My Music", "Secrets.txt"]
+};
 
+let currentHistory = ["C:\\"];
