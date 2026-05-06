e463e3c88de89f10e66ed56538723cd32262c25a
Glackso <jabrokecool@gmail.com>
2026-05-06 12:59:39 -0500

    Update filesystem.js

--- js/filesystem.js
+++ js/filesystem.js
@@ -1,7 +1,16 @@
 const driveC = {
-    "C:\\": ["Windows", "Documents", "Program Files"],
-    "C:\\Windows": ["System32", "Web", "Media"],
-    "C:\\Documents": ["My Pictures", "My Music", "Secrets.txt"]
+    "C:\\": [
+        { name: "Windows", type: "folder" },
+        { name: "Documents", type: "folder" },
+        { name: "Readme.txt", type: "file", content: "Welcome to Windows XP!\n\nThis is a simulation built for GitHub." }
+    ],
+    "C:\\Windows": [
+        { name: "System32", type: "folder" },
+        { name: "notepad.exe", type: "file", content: "You found the notepad executable!" }
+    ],
+    "C:\\Documents": [
+        { name: "Ideas.txt", type: "file", content: "- Build a taskbar\n- Add a Start Menu\n- Win the internet" }
+    ]
 };
 
 let currentHistory = ["C:\\"];
