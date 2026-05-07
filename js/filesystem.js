fed81c2ae4c81d9c6e0328598d9bf44537805782
Glackso <jabrokecool@gmail.com>
2026-05-07 11:59:26 -0500

    Update filesystem.js

--- js/filesystem.js
+++ js/filesystem.js
@@ -14,3 +14,38 @@ const driveC = {
 };
 
 let currentHistory = ["C:\\"];
+
+function renderFiles(path) {
+    const viewer = document.getElementById('file-viewer');
+    const pathDisplay = document.getElementById('current-path');
+    if (!viewer) return;
+
+    viewer.innerHTML = "";
+    pathDisplay.innerText = path;
+
+    const files = driveC[path] || [];
+    files.forEach(item => {
+        const li = document.createElement('li');
+        li.className = "file-item";
+        const icon = item.type === 'folder' ? 'explorer.png' : 'notepad.png';
+        li.innerHTML = `<img src="https://winxp.vercel.app/icons/${icon}" width="16"> ${item.name}`;
+        
+        li.onclick = () => {
+            if (item.type === 'folder') {
+                const newPath = path === "C:\\" ? `${path}${item.name}` : `${path}\\${item.name}`;
+                currentHistory.push(newPath);
+                renderFiles(newPath);
+            } else {
+                notepadApp.openExisting(item);
+            }
+        };
+        viewer.appendChild(li);
+    });
+}
+
+function goBack() {
+    if (currentHistory.length > 1) {
+        currentHistory.pop();
+        renderFiles(currentHistory[currentHistory.length - 1]);
+    }
+}
