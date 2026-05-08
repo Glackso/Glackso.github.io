3d8813c36ad2719326af9dd10fb93b78f0a7d994
Glackso <jabrokecool@gmail.com>
2026-05-08 09:38:07 -0500

    Update filesystem.js

--- js/filesystem.js
+++ js/filesystem.js
@@ -1,59 +1,17 @@
-const driveC = {
-    "C:\\": [
-        { name: "Windows", type: "folder" },
-        { name: "Documents", type: "folder" },
-        { name: "My Notes", type: "folder" }
-    ],
-    "C:\\Windows": [
-        { name: "System32", type: "folder" }
-    ],
-    "C:\\Documents": [
-        { name: "Readme.txt", type: "file", content: "Welcome to your new XP Simulator!" }
-    ],
-    "C:\\My Notes": []
-};
-
-let currentHistory = ["C:\\"];
-
-function renderFiles(path) {
-    const viewer = document.getElementById('file-viewer');
-    const pathDisplay = document.getElementById('current-path');
-    if (!viewer) return;
-
-    viewer.innerHTML = "";
-    pathDisplay.innerText = path;
-
-    const files = driveC[path] || [];
-    files.forEach(item => {
-        const li = document.createElement('li');
-        li.className = "file-item";
-        
-        // Logical check for icons
-        let iconName = 'documents.png'; // Default for files
-        if (item.type === 'folder') {
-            iconName = 'folder.png';
-        }
-        
-        const iconPath = `assets/icons/16/${iconName}`;
-        
-        li.innerHTML = `<img src="${iconPath}" width="16" onerror="this.src='https://winxp.vercel.app/icons/${item.type === 'folder' ? 'explorer.png' : 'notepad.png'}'"> ${item.name}`;
-        
-        li.onclick = () => {
-            if (item.type === 'folder') {
-                const newPath = path.endsWith('\\') ? `${path}${item.name}` : `${path}\\${item.name}`;
-                currentHistory.push(newPath);
-                renderFiles(newPath);
-            } else {
-                notepadApp.openExisting(item);
-            }
-        };
-        viewer.appendChild(li);
-    });
-}
-
-function goBack() {
-    if (currentHistory.length > 1) {
-        currentHistory.pop();
-        renderFiles(currentHistory[currentHistory.length - 1]);
+const SystemFiles = {
+    computer: {
+        title: "My Computer",
+        icon: "assets/icons/16/computer.png",
+        content: `
+            <div class="xp-grid">
+                <div class="folder-item">
+                    <img src="assets/icons/32/folder.png">
+                    <p>Shared Documents</p>
+                </div>
+                <div class="folder-item">
+                    <img src="assets/icons/32/folder.png">
+                    <p>Local Disk (C:)</p>
+                </div>
+            </div>`
     }
-}
+};
