65419960df8072f56f6d9d497d9a9c51649e9d99
Glackso <jabrokecool@gmail.com>
2026-05-08 10:35:46 -0500

    Update apps.js

--- js/apps.js
+++ js/apps.js
@@ -1,32 +1,20 @@
 const AppRegistry = {
-    computer: {
+    my_computer: {
         title: "My Computer",
-        width: "400px",
-        icon: "assets/icons/16/computer.png",
+        icon: "assets/icons/32/computer.png",
         getContent: () => `
-            <div class="field-row" style="padding: 20px; justify-content: center;">
-                <div class="drive-item" style="text-align:center">
-                    <img src="assets/icons/32/computer.png"><br>Local Disk (C:)
-                </div>
-            </div>`
+            <div class="status-bar">
+                <p class="status-bar-field">3 objects</p>
+            </div>
+            <div class="folder-view">
+                <div class="item" onclick="openFolder('C:')">Local Disk (C:)</div>
+                <div class="item" onclick="openFolder('D:')">CD Drive (D:)</div>
+            </div>
+        `
     },
-    counter: {
-        title: "Counter.exe",
-        width: "250px",
-        getContent: () => `
-            <p style="text-align: center">Current count: <span id="count-val">0</span></p>
-            <div class="field-row" style="justify-content: center">
-                <button onclick="document.getElementById('count-val').innerText++">+</button>
-                <button onclick="document.getElementById('count-val').innerText--">-button>
-                <button onclick="document.getElementById('count-val').innerText = 0">0</button>
-            </div>`
-    },
-    cmd: {
-        title: "Command Prompt",
-        width: "500px",
-        getContent: () => `
-            <div style="background:black; color:white; font-family:monospace; padding:10px; height:200px;">
-                Microsoft Windows XP [Version 5.1.2600]<br>C:\\Documents and Settings\\Admin> _
-            </div>`
+    notepad: {
+        title: "Notepad",
+        icon: "assets/icons/32/notepad.png",
+        getContent: () => `<textarea style="width:100%; height:200px;"></textarea>`
     }
 };
