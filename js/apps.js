4fa93328ec76fa9a13fb70544d793bc44ab13dac
Glackso <jabrokecool@gmail.com>
2026-05-08 10:30:01 -0500

    Update apps.js

--- js/apps.js
+++ js/apps.js
@@ -1,23 +1,32 @@
 const AppRegistry = {
     computer: {
         title: "My Computer",
+        width: "400px",
         icon: "assets/icons/16/computer.png",
-        getContent: () => {
-            return `
-                <div class="folder-view">
-                    <div class="drive-item"><img src="assets/icons/32/computer.png"> Local Disk (C:)</div>
-                    <div class="drive-item"><img src="assets/icons/32/folder.png"> My Documents</div>
-                </div>`;
-        }
+        getContent: () => `
+            <div class="field-row" style="padding: 20px; justify-content: center;">
+                <div class="drive-item" style="text-align:center">
+                    <img src="assets/icons/32/computer.png"><br>Local Disk (C:)
+                </div>
+            </div>`
     },
-    notepad: {
-        title: "Notepad",
-        icon: "assets/icons/16/notepad.png",
-        getContent: () => `<textarea class="notepad-text"></textarea>`
+    counter: {
+        title: "Counter.exe",
+        width: "250px",
+        getContent: () => `
+            <p style="text-align: center">Current count: <span id="count-val">0</span></p>
+            <div class="field-row" style="justify-content: center">
+                <button onclick="document.getElementById('count-val').innerText++">+</button>
+                <button onclick="document.getElementById('count-val').innerText--">-button>
+                <button onclick="document.getElementById('count-val').innerText = 0">0</button>
+            </div>`
     },
     cmd: {
         title: "Command Prompt",
-        icon: "assets/icons/16/cmd.png",
-        getContent: () => `<div class="cmd-box">Microsoft Windows XP [Version 5.1.2600]<br>(C) Copyright 1985-2001 Microsoft Corp.<br><br>C:\\> _</div>`
+        width: "500px",
+        getContent: () => `
+            <div style="background:black; color:white; font-family:monospace; padding:10px; height:200px;">
+                Microsoft Windows XP [Version 5.1.2600]<br>C:\\Documents and Settings\\Admin> _
+            </div>`
     }
 };
