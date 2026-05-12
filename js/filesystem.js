cc665f2a163b0887febda40c20d4a136b73da1d1
Glackso <jabrokecool@gmail.com>
2026-05-12 14:15:02 -0500

    Update filesystem.js

--- js/filesystem.js
+++ js/filesystem.js
@@ -4,7 +4,7 @@ const driveC = {
         { name: "Documents", type: "folder" },
         { name: "My Notes", type: "folder" },
         { name: "Recent", type: "folder" },
-        { name: "Readme.txt", type: "file", content: "Welcome to Windows XP Simulator!\n\nThis is a functional file system." }
+        { name: "Readme.txt", type: "file", content: "Welcome to Windows XP Simulator!\n\nThis is a functional file system. Fun fact this note was savalged!" }
     ],
     "C:\\\\Documents": [
         { name: "Pictures", type: "folder" },
