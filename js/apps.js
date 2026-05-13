d99cf8c3c1657df8cf4a17a62fc8d24fa600bef9
Glackso <jabrokecool@gmail.com>
2026-05-12 18:35:02 -0700

    Update apps.js

--- js/apps.js
+++ js/apps.js
@@ -1,25 +1,12 @@
-// Register "My Computer"
-AppManager.registerApp('myComputer', {
-    name: 'My Computer',
-    // Point these to your actual icon files in the assets folder
-    icon16: 'assets/icons/16/computer.png', 
-    icon32: 'assets/icons/32/computer.png',
-    
-    // The HTML that goes inside the window
-    contentHTML: `
-        <div style="padding: 15px; height: 100%; background: #fff;">
-            <h3>Hard Disk Drives</h3>
-            <hr>
-            <div style="display: flex; gap: 10px; align-items: center; margin-top: 10px;">
-                <img src="assets/icons/32/drive.png" onerror="this.src='https://via.placeholder.com/32'" alt="Drive">
-                <div>
-                    <strong>Local Disk (C:)</strong><br>
-                    <small>0 bytes free of 0 bytes</small>
-                </div>
-            </div>
+AppManager.registry['myComputer'] = {
+    name: "My Computer",
+    width: "500px",
+    content: `
+        <div class="status-bar">
+          <p class="status-bar-field">3 objects</p>
+          <p class="status-bar-field">My Computer</p>
         </div>
+        <div class="drive-list">
+            </div>
     `
-});
-
-// To add another app later, just do:
-// AppManager.registerApp('minesweeper', { ... });
+};
