777ad3e72fb086fde8af45efa4233561e4280475
Glackso <jabrokecool@gmail.com>
2026-05-12 17:57:22 -0700

    Update filesystem.js

--- js/filesystem.js
+++ js/filesystem.js
@@ -1,16 +1,1 @@
-const driveC = {
-    "C:\\": [
-        { name: "Windows", type: "folder" },
-        { name: "Documents", type: "folder" },
-        { name: "My Notes", type: "folder" },
-        { name: "Recent", type: "folder" },
-        { name: "Readme.txt", type: "file", content: "Welcome to Windows XP Simulator!\n\nThis is a functional file system. Fun fact this note was savalged!" }
-    ],
-    "C:\\\\Documents": [
-        { name: "Pictures", type: "folder" },
-        { name: "Music", type: "folder" }
-    ],
-    "C:\\\\My Notes": [
-        { name: "Todo.txt", type: "file", content: "1. Fix My Computer\n2. Add Minesweeper\n3. Profit" }
-    ]
-};
+Directory like C:// for my computer
