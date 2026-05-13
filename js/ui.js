15ea3b8473aca0ae90cbfe053e54f6a1c0d1a03c
Glackso <jabrokecool@gmail.com>
2026-05-12 17:58:10 -0700

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,14 +1,1 @@
-function renderShortcuts() {
-    const grid = document.getElementById('icon-grid');
-    grid.innerHTML = `
-        <div class="shortcut" ondblclick="AppManager.open('computer')">
-            <img src="assets/icons/32/computer.png">
-            <p>My Computer</p>
-        </div>
-    `;
-}
-
-function toggleStartMenu() {
-    const m = document.getElementById('start-menu');
-    m.style.display = m.style.display === 'none' ? 'block' : 'none';
-}
+ui for startmenu and etc
